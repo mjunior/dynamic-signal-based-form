6bf340ca68a2ec7ee3d6fe26d5f6e5e44c879763
Mauricio Junior <mauriciojs.junior@gmail.com>
2025-10-25 02:51:33 -0300

    tailwind init ai

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#1DB496',
+          hover: '#21CAA8',
+          light: '#DBF6F0',
+          'most-light': '#F2FDFB',
+        },
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
